b2343b75ad6342a1b78d2b842b62de1181628e56
gabrielgoisandrade <gabriel.gois.andrade14@gmail.com>
2026-01-21 15:50:14 -0300

    subindo projeto devstore

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  images: {
+    remotePatterns: [new URL("https://github.com/gabrielgoisandrade.png")], // * permite usar endereços externos no Image do next.
+  },
+};
+
+export default nextConfig;
---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+  title: "devstore",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br" className={inter.variable}>
+      <body
+        className='antialiased bg-zinc-950 text-zinc-50'
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
